0f135c49cc3e8c6488670b0ef6c5b5610fcdca24
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-07-22 10:38:49 +0200

    localStorage OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Contact } from './models/contact.model';
 
@@ -7,7 +7,7 @@ import { Contact } from './models/contact.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   contactForm: FormGroup;
   contacts: Contact[] = [];
   selectedContact: Contact | null = null;
@@ -22,19 +22,36 @@ export class AppComponent {
     });
   }
 
+  ngOnInit() {
+    this.loadContacts();
+  }
+
   ajouterContact() {
     if (this.contactForm.valid) {
       const newContact: Contact = this.contactForm.value;
       this.contacts.push(newContact);
+      this.saveContacts();
       this.contactForm.reset();
     }
   }
 
   supprimerContact(index: number) {
     this.contacts.splice(index, 1);
+    this.saveContacts();
   }
 
   selectContact(contact: Contact) {
     this.selectedContact = contact;
+  }
+
+  saveContacts() {
+    localStorage.setItem('contacts', JSON.stringify(this.contacts));
+  }
+
+  loadContacts() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.contacts = JSON.parse(storedContacts);
+    }
   }
 }
